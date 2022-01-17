4373b52fa8c141eff5ad92fd55c4f7c0eacbc635
Soma Dipti <55309077+731723@users.noreply.github.com>
2022-01-17 19:50:09 +0530

    Update GCD.py

--- DSandAlgo/GCD.py
+++ DSandAlgo/GCD.py
@@ -1,6 +1,7 @@
-# to find GCD of a number using euclid Algorithm
-# gcd(m,n) m=qn+r if m = ad and n=bd where d is common divisor between m and n , then ad=qbd+r and r is divisible by d
-def findgcd(m, n):
+# to find GCD of a number using euclid Algorithm : m and n will have atleast 1 as common divisor and if d is largest common divisor between m and n then m=ad and n=bd,
+#m-n=ad-bd=(a-b)d therefore gcd(m,n)=gcd(n,m-n)
+# gcd(m,n): the number m can be written as m=qn+r ---(i)  replacing this in (i) ad=qbd+r and so r will also be divisible by d
+    # assume m>n if not interchange
     if m < n:
         (m, n) = (n, m)
     while m % n != 0:
